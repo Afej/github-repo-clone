cc1c0fd537c42388798bf779dfb5435b53d1ae65
Afej <afekurojoshua@gmail.com>
2020-11-18 17:54:21 +0100

    added relative time function

--- js/populateUI.js
+++ js/populateUI.js
@@ -16,6 +16,45 @@ const formatNumber = (number) => {
   return formattedNumber;
 };
 
+const getRelativeTime = (updatedAt) => {
+  const now = Date.now();
+  const then = Date.parse(updatedAt);
+
+  const [nowWeekday, nowMonth, nowDay, nowYear] = new Date(now)
+    .toString()
+    .split(" ");
+
+  const [thenWeekday, thenMonth, thenDay, thenYear] = new Date(then)
+    .toString()
+    .split(" ");
+
+  const elapsed = now - then;
+
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+
+  if (elapsed < msPerMinute) {
+    const time = Math.round(elapsed / 1000);
+    return time + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    const time = Math.round(elapsed / msPerMinute);
+    return time + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    const time = Math.round(elapsed / msPerHour);
+    return `${time} ${time > 1 ? "hours" : "hour"} ago`;
+  } else if (elapsed < msPerMonth) {
+    const time = Math.round(elapsed / msPerDay);
+    return `${time} ${time > 1 ? "days" : "day"} ago`;
+  } else {
+    return `on ${thenMonth} ${thenDay}${
+      thenYear < nowYear ? ", " + thenYear : ""
+    }`;
+  }
+};
+
+// set required field with data
 const setReqData = {
   setName(fullname) {
     name.innerText = fullname.trim();
@@ -151,7 +190,9 @@ const setReqData = {
             </span>
 
             <span class="updated-at">
-              Updated <span class="relative-time"> ${pushedAt}</span>
+              Updated <span class="relative-time"> ${getRelativeTime(
+                pushedAt
+              )}</span>
             </span>
           </div>
         </div>
